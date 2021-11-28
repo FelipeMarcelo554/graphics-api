2f19f2f07281dd82d6b52ff8168c28ed629256e3
Felipe M <felipemarcelo554@gmail.com>
2021-11-28 12:59:48 -0300

    CORS fix

--- src/main/java/com/felipemarcelo554/graphicsAPI/config/CorsConfiguration.java
+++ src/main/java/com/felipemarcelo554/graphicsAPI/config/CorsConfiguration.java
@@ -1,8 +1,10 @@
 package com.felipemarcelo554.graphicsAPI.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
